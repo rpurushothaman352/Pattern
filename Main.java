ee072cbfdf8726495600c81ebc65e52fc2d64ba9
rpurushothaman352 <84661010+rpurushothaman352@users.noreply.github.com>
2022-09-30 11:02:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        pyramid.upleftsidepyramid();
+        System.out.println();
+        pyramid.downleftsidepyramid();
+        System.out.println();
+        pyramid.uprightsidepyramid();
+        System.out.println();
+        pyramid.downrighttsidepyramid();
+        System.out.println();
+        pyramid.uptsidepyramid();
+        System.out.println();
+        pyramid.upsidepyramid3();
+
+    }
+}
